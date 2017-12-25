50d152fb1e6f7ce2d70863d5340a6fa3dfb83219
звездочёт <mykaralw@yandex.ru>
2017-12-25 04:06:50 +0300

    0.20171225
    pyformat -a -i --remove-all-unused-imports --remove-unused-variables

--- trimimagetopdf.py
+++ trimimagetopdf.py
@@ -16,75 +16,98 @@
 
 import argparse
 import Image
-import ImageTk
-import Tkinter
-import sys
-import os
 from reportlab.pdfgen.canvas import Canvas
 from PIL import Image, ImageChops, ImageFilter
 
-PROGNAME='TrimImagetoPDF'
-VERSION='0.20171224'
+PROGNAME = 'TrimImagetoPDF'
+VERSION = '0.20171225'
+
 
 def export_pdf(imgname, autotm, default_dpi, outfile):
-  """Trim the image and creates a PDF with the same size"""
-  if outfile == '':
-    outfile = '%s.pdf'%(imgname)
-  outtrim = '%s-trim.png'%(outfile)
-  pdf = Canvas(outfile, pageCompression=1)
-  dpi = default_dpi
-  im = Image.open(imgname)
-  w, h = im.size
-  width = round(w * 72.0 / dpi, 3)
-  height = round(h * 72.0 / dpi, 3)
-  pdf.setPageSize((width, height))
-  if autotm:
-    trimbox = autocrop(im, 255)
-  else:
-    if im.mode == "RGB":
-      trimbox = trim(im, (255,255,255))
+    """Trim the image and creates a PDF with the same size."""
+    if outfile == '':
+        outfile = '%s.pdf' % (imgname)
+    outtrim = '%s-trim.png' % (outfile)
+    pdf = Canvas(outfile, pageCompression=1)
+    dpi = default_dpi
+    im = Image.open(imgname)
+    w, h = im.size
+    width = round(w * 72.0 / dpi, 3)
+    height = round(h * 72.0 / dpi, 3)
+    pdf.setPageSize((width, height))
+    if autotm:
+        trimbox = autocrop(im, 255)
     else:
-      trimbox = trim(im, 255)
-  if trimbox:
-    print trimbox
-    x1, y1, x2, y2 = trimbox
-    wt = round((x2 - x1) * 72.0 / dpi, 3)
-    ht = round((y2 - y1) * 72.0 / dpi, 3)
-    x = round(x1 * 72.0 /dpi, 3)
-    y = height - round(y2 * 72.0 / dpi, 3)
-    trimim=im.crop(trimbox)
-    trimim.save(outtrim)
-    pdf.drawImage(outtrim, x, y, width=wt, height=ht)
-  else:
-    # found no content
-    raise ValueError("cannot trim; image was empty")
-  pdf.showPage()
-  pdf.save()
+        if im.mode == 'RGB':
+            trimbox = trim(im, (255, 255, 255))
+        else:
+            trimbox = trim(im, 255)
+    if trimbox:
+        print trimbox
+        x1, y1, x2, y2 = trimbox
+        wt = round((x2 - x1) * 72.0 / dpi, 3)
+        ht = round((y2 - y1) * 72.0 / dpi, 3)
+        x = round(x1 * 72.0 / dpi, 3)
+        y = height - round(y2 * 72.0 / dpi, 3)
+        trimim = im.crop(trimbox)
+        trimim.save(outtrim)
+        pdf.drawImage(outtrim, x, y, width=wt, height=ht)
+    else:
+        # found no content
+        raise ValueError('cannot trim; image was empty')
+    pdf.showPage()
+    pdf.save()
+
 
 def trim(im, border):
-  bg = Image.new(im.mode, im.size, border)
-  diff = ImageChops.difference(im, bg)
-  bbox = diff.getbbox()
-  return bbox
+    bg = Image.new(im.mode, im.size, border)
+    diff = ImageChops.difference(im, bg)
+    bbox = diff.getbbox()
+    return bbox
+
 
 def autocrop(im, border):
-  bw = im.convert("1")
-  bw = bw.filter(ImageFilter.MedianFilter)
-  bg = Image.new("1", im.size, border)
-  diff = ImageChops.difference(bw, bg)
-  bbox = diff.getbbox()
-  return bbox
+    bw = im.convert('1')
+    bw = bw.filter(ImageFilter.MedianFilter)
+    bg = Image.new('1', im.size, border)
+    diff = ImageChops.difference(bw, bg)
+    bbox = diff.getbbox()
+    return bbox
+
 
 def print_version():
-  print PROGNAME
-  print VERSION
+    print PROGNAME
+    print VERSION
 
-if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description="Trim the image and creates a PDF with the same size")
-  parser.add_argument('-a', '--auto', action='store_true', default=False, help='autotrim, bw mode, default False')
-  parser.add_argument('-d', '--dpi', metavar='dpi', type=int, default=300, help='dpi for pdf output, default is 300')
-  parser.add_argument('-o', '--outfile', metavar='outfile', type=str, default='', help='output pdf name, default imgname.pdf')
-  parser.add_argument('-v', '--version', action='version', version=print_version())
-  parser.add_argument("imgname", help="image file name")
-  args = parser.parse_args()
-  export_pdf(args.imgname, args.auto, args.dpi, args.outfile)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Trim the image and creates a PDF with the same size')
+    parser.add_argument(
+        '-a',
+        '--auto',
+        action='store_true',
+        default=False,
+        help='autotrim, bw mode, default False')
+    parser.add_argument(
+        '-d',
+        '--dpi',
+        metavar='dpi',
+        type=int,
+        default=300,
+        help='dpi for pdf output, default is 300')
+    parser.add_argument(
+        '-o',
+        '--outfile',
+        metavar='outfile',
+        type=str,
+        default='',
+        help='output pdf name, default imgname.pdf')
+    parser.add_argument(
+        '-v',
+        '--version',
+        action='version',
+        version=print_version())
+    parser.add_argument('imgname', help='image file name')
+    args = parser.parse_args()
+    export_pdf(args.imgname, args.auto, args.dpi, args.outfile)
